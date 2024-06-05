b664ab513d28114bb1fb52cfcefd589bbd5cd990
asdiAdi <carltimothyadi@gmail.com>
2024-06-05 23:40:43 +0800

    Add assets

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from 'react-router-dom'
+import React, { lazy } from 'react'
+
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: lazy(() => import('../App.tsx')),
+//     },
+// ])
